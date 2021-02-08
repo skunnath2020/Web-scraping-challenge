3a657ea2ef3222827bee627ad2095b9a14462a6d
skunnath2020 <sskunnath@gmail.com>
2021-02-08 14:25:58 -0500

    Python code wip

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -6,7 +6,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 # Route to render index.html template using data from Mongo
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_dict = mongo.db.mars_dict.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars=mars_dict)
 
 
 # Route that will trigger the scrape function
@@ -25,10 +25,9 @@ def home():
 def scrape():
 
     # Run the scrape function
-    costa_data = scrape_costa.scrape_info()
-
+    mars_data = scrape_mars.scrape()
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
+    mongo.db.mars_dict.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
