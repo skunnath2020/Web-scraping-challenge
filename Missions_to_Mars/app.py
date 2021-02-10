4b722391f445cd0ebe1f4f37a2defffba24e928d
skunnath2020 <sskunnath@gmail.com>
2021-02-10 17:04:35 -0500

    wip

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-
+import os
 # Create an instance of Flask
 app = Flask(__name__)
-
+app._static_folder = "static"
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
